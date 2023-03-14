66c29c78b4788665194a41ba12f1fbf9e88dc004
Mark <1405144502@qq.com>
2023-03-14 19:23:58 +0800

    hot-fix first

--- src/hello.java
+++ src/hello.java
@@ -2,6 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("word");
+        System.out.println("hot-fix");
     }
 }
 
